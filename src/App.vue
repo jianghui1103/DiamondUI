4317deae0794e428b0be6b34605ed7d5593b1fa6
jianghui1103 <1219936992@qq.com>
2020-09-02 22:27:25 +0800

    实现aside 切换

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
 <router-view />
 </template>
 
-<script>
+<script lang="ts">
+import {
+    ref,
+    provide
+} from 'vue';
 export default {
     name: 'App',
+    setup() {
+        const menuVisible = ref(false) // set
+        provide('xxx', menuVisible) // 所有的都可以使用
+    }
 }
 </script>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,16 +1,24 @@
 <template>
 <div>
     <Topnav />
-    <div class="topnav">
-        <div class="logo">
-
-        </div>
-        <div class="menu">
-
-        </div>
-    </div>
     <div class="content">
-        <aside>边栏</aside>
+        <aside v-if="menuVisible">
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <router-link to="/doc/Switch">Switch 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/button">Button 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/dialog">Dialog 组件</router-link>
+                </li>
+                <li>
+                    <router-link to="/doc/tabs">Tabs 组件</router-link>
+                </li>
+            </ol>
+        </aside>
         <main>主内容</main>
     </div>
 </div>
@@ -18,9 +26,42 @@
 
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
+import {
+    inject,
+    Ref
+} from 'vue';
 export default {
     components: {
         Topnav
+    },
+    setup() {
+        const menuVisible = inject < Ref < boolean > > ('xxx') // get
+        console.log('Doc 获取的menuVisible', menuVisible.value)
+        return {
+            menuVisible
+        }
     }
 }
 </script>
+
+<style lang="scss" scoped>
+aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+
+    >h2 {
+        margin-bottom: 4px;
+    }
+
+    >ol {
+        >li {
+            padding: 4px 0;
+        }
+    }
+}
+</style>
