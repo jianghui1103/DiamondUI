83b69508c670e5b44f44f06eebe761c2fcb22748
jianghui1103 <1219936992@qq.com>
2020-09-02 22:34:19 +0800

    添加toggle button

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,6 +5,7 @@
         <li>菜单1</li>
         <li>菜单2</li>
     </ul>
+    <span class="toggleMenu"></span>
 </div>
 </template>
 
@@ -33,6 +34,8 @@ export default {
     padding: 16px;
     position: relative;
     z-index: 10;
+    justify-content: center;
+    align-items: center;
 
     >.logo {
         max-width: 6em;
@@ -46,6 +49,27 @@ export default {
 
         >li {
             margin: 0 1em;
+        }
+    }
+
+    >.toggleMenu {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    @media (max-width: 500px) {
+        >.menu {
+            display: none;
+        }
+
+        >.logo {
+            margin: 0 auto;
         }
     }
 }
