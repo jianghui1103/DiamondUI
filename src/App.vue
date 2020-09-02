fa178043c0df48bc1b1e0a251d36f9c0d46bcc2a
jianghui1103 <1219936992@qq.com>
2020-09-03 00:22:47 +0800

    手机端修改

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,10 @@ import {
 export default {
     name: 'App',
     setup() {
-        const menuVisible = ref(false) // set
+        const width = document.documentElement.clientWidth;
+        console.log(width)
+        const menuVisible = ref(width <= 500 ? false : true) // set
+        console.log(menuVisible)
         provide('menuVisible', menuVisible) // 所有的都可以使用
     }
 }
