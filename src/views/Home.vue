96e29bffae5ea414730f5a683addb951b414b8e5
jianghui1103 <1219936992@qq.com>
2020-09-02 21:46:49 +0800

    Home css修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,10 +13,32 @@
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from '../components/Topnav.vue';
 export default {
     components: {
         Topnav
     }
 }
 </script>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
