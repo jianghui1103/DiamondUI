2f724aa1063f2c59cfdff7e02c5553c2b6d4bd65
jianghui <jianghui@upjiajia.com>
2020-09-04 10:01:42 +0800

    创建dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
-        <p>Dialog 的文档</p>
+        dialog 实例
     </div>
+    <h1>实例1</h1>
+    <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+    components: {
+        Dialog
+    }
+}
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Diamond-dialog-overlay"></div>
+    <div class="Diamond-dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行子</p>
+            <p>第二行子</p>
+        </main>
+        <footer>
+            <Button>确定</Button>
+            <Button>取消</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from './Button.vue'
+export default {
+    components: {Button}
+}
+</script>
