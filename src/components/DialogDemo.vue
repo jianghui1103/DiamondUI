988cb027dd9412f216b4a26047a565b5410c8377
jianghui <jianghui@upjiajia.com>
2020-09-04 16:51:19 +0800

    dialog 点击事件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,14 +3,32 @@
         dialog 实例
     </div>
     <h1>实例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">toggle</Button>
+    <!-- <Dialog :visible="isVisible" @update:visible = "isVisible = $event"></Dialog> -->
+    <Dialog v-model:visible="isVisible" :closeOnClickOverlay="false" :cancel="cancel" :ok="ok"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import { ref } from 'vue';
 export default {
     components: {
-        Dialog
+        Dialog,
+        Button
+    },
+    setup() {
+        const isVisible = ref(false)
+        const toggle = ()=>{
+            isVisible.value = !isVisible.value
+        }
+        const cancel = ()=> {
+            console.log('关闭')
+        }
+        const ok = ()=> {
+            console.log('确认')
+        }
+        return { isVisible,toggle,cancel,ok }
     }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,133 @@
 <template>
-    <div class="Diamond-dialog-overlay"></div>
-    <div class="Diamond-dialog-wrapper">
-        <header>标题</header>
-        <main>
-            <p>第一行子</p>
-            <p>第二行子</p>
-        </main>
-        <footer>
-            <Button>确定</Button>
-            <Button>取消</Button>
-        </footer>
-    </div>
+    <template v-if="visible">
+        <div class="Diamond-dialog-overlay" @click="onClickOverlay"></div>
+        <div class="Diamond-dialog-wrapper">
+            <div class="Diamond-dialog">
+                <header>标题
+                    <span class="Diamond-dialog-close" @click="close"></span>
+                </header>
+                <main>
+                    <p>第一行子</p>
+                    <p>第二行子</p>
+                </main>
+                <footer>
+                    <Button level="main" @click="ok">确定</Button>
+                    <Button @click="cancel">取消</Button>
+                </footer>
+            </div>
+        </div>
+    </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue'
 export default {
+    props: {
+        visible:{
+            type:Boolean,
+            default: false
+        },
+        closeOnClickOverlay: {
+            type:Boolean,
+            default: true
+        },
+        ok: {
+          type:Function,
+        },
+        cancel: {
+          type:Function,
+        },
+    },
+    setup(props, context) {
+        const close = ()=> {
+            context.emit('update:visible',false)
+        }
+        const onClickOverlay = ()=> {
+          if(props.closeOnClickOverlay) {
+            close()
+          }
+        }
+        const ok = ()=> {
+          if( props.ok?.() !== false ) {
+          // if( props.ok && props.ok() !== false ) {
+            close()
+          }
+        }
+        const cancel = ()=> {
+          if( props.cancel?.() !== false ) {
+          // if( props.ok && props.ok() !== false ) {
+            close()
+          }
+        }
+        return {close,onClickOverlay,ok,cancel}
+    },
     components: {Button}
 }
 </script>
+
+<style lang="scss" scoped>
+$radius: 4px;
+$border-color: #d9d9d9;
+.Diamond-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+  >header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+  >main {
+    padding: 12px 16px;
+  }
+  >footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
