aa1407987944901e88c9953a6ac93c7871af1782
jianghui1103 <1219936992@qq.com>
2020-09-21 20:57:56 +0800

    高亮样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -96,7 +96,7 @@ export default {
 aside {
     background: lightblue;
     width: 150px;
-    padding: 16px;
+    padding: 16px 0;
     position: fixed;
     top: 0;
     left: 0;
@@ -106,11 +106,20 @@ aside {
 
     >h2 {
         margin-bottom: 4px;
+        padding: 0 16px;
     }
 
     >ol {
         >li {
-            padding: 4px 0;
+
+            a {
+                display: block;
+                padding: 4px 16px;
+            }
+
+            .router-link-active {
+                background: white;
+            }
         }
     }
 
