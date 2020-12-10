55c8466759fd2a5d6aa5359dd223c88e3d216944
zhangbin01 <Zhang2012>
2020-12-10 20:01:35 +0800

    增加clear

--- src/components/InputDemo/Input2.demo.vue
+++ src/components/InputDemo/Input2.demo.vue
@@ -1,15 +1,15 @@
 <demo>
-常规使用
+清除输入框
 </demo>
 <template>
 <div>
-    <DInput v-model="input" placeholder="输入框"> </DInput>
+    <DInput v-model="input" placeholder="请输入内容" clearable> </DInput>
 </div>
 </template>
 
 <script lang="ts">
 import DInput from "../../lib/Input/Input.vue";
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 export default {
   components: {
     DInput
--- src/lib/Input/Input.vue
+++ src/lib/Input/Input.vue
@@ -1,15 +1,27 @@
 <template>
+  <div class="Diamond-input">
     <input
-        class="Diamond-input"
+        class="Diamond-input__inner"
         :class="classes"
         :placeholder="placeholder"
         :type="type"
         :disabled="disabled"
-    >
+    />
+    <span class="Diamond-input__suffix" >
+      <span class="Diamond-input__suffix-inner">
+        <svg>
+          <use xlinkHref=''></use>
+        </svg>
+        
+
+      </span>
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
 import { computed } from 'vue';
+import clear from '../../icons/clear.svg'
 export default {
     props: {
         placeholder: {
@@ -24,12 +36,13 @@ export default {
           type: Boolean,
           default: false
         },
-        loading: {
+        clearable: {
           type: Boolean,
           default: false
         }
     },
     setup(props) {
+      console.log(clear)
       const { disabled } = props;
       const classes = computed(()=>{
         return {
@@ -49,31 +62,37 @@ $blue: #40a9ff;
 $radius: 4px;
 $red: red;
 $grey: grey;
-.Diamond-input {
-  box-sizing: border-box;
-  height: $h;
-  padding: 0 12px;
-  cursor: pointer;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  white-space: nowrap;
-  background: white;
-  color: $color;
-  border: 1px solid $border-color;
-  border-radius: $radius;
-  box-shadow: 0 1px 0 fade-out(black, 0.95);
-  transition: background 250ms;
-  &:hover,
-  &:focus {
-    border-color: $blue;
-  }
-  &.Diamond-input-disabled {
+.Diamond-input{
+  position: relative;
+  font-size: 14px;
+  display: inline-block;
+  width: 100%;
+  .Diamond-input__inner {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+    transition: background 250ms;
     &:hover,
     &:focus {
-        border-color: $border-color;
+      border-color: $blue;
+    }
+    &.Diamond-input-disabled {
+      &:hover,
+      &:focus {
+          border-color: $border-color;
+      }
     }
   }
-
 }
+
 </style>
