c4763d7fc4ea49d36d0d19e6fe8ef7d89a49c757
zhangbin01 <Zhang2012>
2021-01-05 16:38:03 +0800

    复选框可选个数

---
+++ src/components/Checkbox3.demo.vue
@@ -0,0 +1,31 @@
+<demo>
+限制可选个数
+</demo>
+<template>
+<div>
+  <checkbox-group v-model:modelValue="checkList" :min="1" :max="2">
+    <Checkbox label="复选框 A"></Checkbox>
+    <Checkbox label="复选框 B"></Checkbox>
+    <Checkbox label="复选框 C"></Checkbox>
+  </checkbox-group>
+</div>
+</template>
+
+<script lang="ts">
+import { ref, computed } from 'vue'
+import Checkbox from "../lib/Checkbox.vue";
+import CheckboxGroup from "../lib/CheckboxGroup.vue";
+export default {
+  components: {
+    Checkbox,
+    CheckboxGroup
+  },
+  setup(){
+    const checkList = ref(['选中且禁用', '复选框 A'])
+    const handleChange = (value)=> {
+      console.log(value)
+    }
+    return { checkList }
+  }
+}
+</script> 
--- src/components/CheckboxDemo.vue
+++ src/components/CheckboxDemo.vue
@@ -4,6 +4,7 @@
     <Demo :component="CheckboxDemo" />
     <Demo :component="CheckboxDemo1" />
     <Demo :component="CheckboxDemo2" />
+    <Demo :component="CheckboxDemo3" />
 </div>
 </template>
 
@@ -12,13 +13,15 @@ import Demo from './Demo.vue'
 import CheckboxDemo from './Checkbox.demo.vue'
 import CheckboxDemo1 from './Checkbox1.demo.vue'
 import CheckboxDemo2 from './Checkbox2.demo.vue'
+import CheckboxDemo3 from './Checkbox3.demo.vue'
 export default {
     components: { Demo },
     setup() {
         return {
             CheckboxDemo,
             CheckboxDemo1,
-            CheckboxDemo2
+            CheckboxDemo2,
+            CheckboxDemo3
         }
     }
 }
